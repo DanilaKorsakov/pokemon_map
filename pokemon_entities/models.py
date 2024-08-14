3ac87231feba094bf02644138022d1a926253518
DanilaKorsakov <kvashnindan@gmail.com>
2024-08-14 15:08:37 +0300

    deleted default values

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -25,11 +25,11 @@ class PokemonEntity(models.Model):
     lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(default=datetime.now, verbose_name='Время появления')
     disappeared_at = models.DateTimeField(default=datetime.now, verbose_name='Время исчезновения')
-    level = models.IntegerField(default=1, verbose_name='Уровень', blank=True)
-    health = models.IntegerField(default=1, verbose_name='Здоровье', blank=True)
-    strength = models.IntegerField(default=1, verbose_name='Сила', blank=True)
-    defence = models.IntegerField(default=1, verbose_name='Защита', blank=True)
-    stamina = models.IntegerField(default=1, verbose_name='Выносливость', blank=True)
+    level = models.IntegerField(verbose_name='Уровень', blank=True)
+    health = models.IntegerField(verbose_name='Здоровье', blank=True)
+    strength = models.IntegerField( verbose_name='Сила', blank=True)
+    defence = models.IntegerField(verbose_name='Защита', blank=True)
+    stamina = models.IntegerField(verbose_name='Выносливость', blank=True)
 
     def __str__(self):
         return f"{self.pokemon}: {self.lat}, {self.lon}"
